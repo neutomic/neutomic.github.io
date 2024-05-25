2b8b029b8de59bf7d538b7ece3eaf3c70c2dd8de
azjezz <azjezz@protonmail.com>
2024-05-25 01:53:08 +0100

    fix copyright

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -96,7 +96,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Neutomic. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Saif Eddin Gmati. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.oneLight,
